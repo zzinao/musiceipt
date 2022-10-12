0e6c2afb19060885791fbf4ad371a3187d0738f7
zzinao <kjn0265@gmail.com>
2022-10-13 01:17:49 +0900

    ✨ 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-
+import MainPage from "./pages/MainPage"
+import ReceiptPage from "./pages/ReceiptPage"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/print" element={<ReceiptPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
